97bdfe8b0934ad98e6e271f8366289565a792e65
Devlimitless <devdevelopercode@gmail.com>
2020-10-04 13:51:54 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Angular 9 Fundamentals
+
+## Getting Started
+
+```
+npm install -g @angular/cli@9.1
+```
+
+```
+npm install
+```
+
+To start the app run
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+ title="Angular 9 Fundamentals Workshop"
+ currentLesson = null;
+  courseLessons = [
+    { title: 'Hello Angular' },
+    { title: 'Component Fundamentals' },
+    { title: 'Template Driven Forms' },
+    { title: 'Angular Services' },
+    { title: 'Server Communication' },
+    { title: 'Component Driven Architecture' },
+    { title: 'Angular Routing' },
+    { title: 'Unit Testing Fundamentals' },
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  selectLesson(lesson){
+    console.log(lesson);
+    this.currentLesson = lesson;
+  }
+
+}
