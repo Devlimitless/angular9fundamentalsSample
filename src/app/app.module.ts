f4fdf4f93cef7941144c62f220df9ac8b92d7f34
Devlimitless <devdevelopercode@gmail.com>
2020-10-04 20:44:18 +0530

    Template Driven Forms phase - 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { CoursesComponent } from './courses/courses.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     MaterialModule,
     HttpClientModule
   ],
--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+
+  selectedCourse = null;
+
   courses = [
     {
       id: 1,
@@ -13,12 +16,48 @@ export class CoursesComponent implements OnInit {
       description: 'Learn the fundamentals of Angular 9',
       percentComplete: 26,
       favorite: true
+    },
+    {
+      id: 2,
+      title: 'Javascript the really really hard parts',
+      description: 'Worship will sentance',
+      percentComplete: 50,
+      favorite: true
     }
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.resetselectedCourse();
+  }
+
+  resetselectedCourse(){
+    const emptyCourse = {
+      id: null,
+      title: '',
+      description: '',
+      percentComplete: 0,
+      favorite: false
+    };
+    this.selectedCourse = emptyCourse;
+  }
+
+  selectCourse(course){
+    console.log(course);
+    this.selectedCourse = course;
+  }
+
+  deleteCourse(courseId){
+    console.log(courseId);
+  }
+
+  cancel(){
+    this.resetselectedCourse();
+  }
+
+  saveCourse(){
+    console.log('saved Course data!')
   }
 
 }
